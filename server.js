e0e4bf0da836d003b1282b3ba48db3949f95834c
Reaveskev <Reaveskev@gmail.com>
2025-01-23 08:54:49 -0800

    Adjusted All API calls

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const corsOptions = {
-  origin:
-    process.env.TEST_API ||
-    process.env.NEXT_PUBLIC_API_URL ||
+  origin: [
+    process.env.TEST_API,
+    process.env.NEXT_PUBLIC_API_URL,
     "http://localhost:3000", // Use environment variable for flexibility
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
